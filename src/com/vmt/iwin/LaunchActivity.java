ad1dbe3f0e35a4c2282da152009422862d2cd910
vmtram <tramvm@gmail.com>
2014-02-23 00:40:29 +0700

    master: first commit

---
+++ src/com/vmt/iwin/LaunchActivity.java
@@ -0,0 +1,46 @@
+package com.vmt.iwin;
+
+import com.vmt.iwin.R.id;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LaunchActivity extends Activity {
+
+    private Button shareButton;
+    private EditText statusText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        statusText = (EditText) findViewById(id.status_text);
+        shareButton = (Button) findViewById(id.share_button);
+        shareButton.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    Toast.makeText(LaunchActivity.this, "Share",
+                            Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.launch, menu);
+        return true;
+    }
+
+}
